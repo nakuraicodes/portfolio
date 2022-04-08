d089c45e43a5d12c790ba73c37cfb9a92d33e455
nakurai <contact@nakuraicodes.com>
2022-04-07 22:22:51 -0400

    updated work exprience and resume pdf

---
+++ src/res/resume/skills.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    category: "Programming",
+    description:
+      "NodeJs, ReactJs, Web Components, React Native, Go (beginner), Python (intermediate)",
+  },
+  {
+    category: "Methods",
+    description: "Agile methodology, UX Interviews, CRISP-DM",
+  },
+  {
+    category: "Languages",
+    description: "French (native), English (fluent)",
+  },
+];
---
+++ src/res/resume/work-experience.js
@@ -0,0 +1,48 @@
+export default [
+  {
+    date: "Mar&nbsp;2021 - Today",
+    title: "Full-Stack Developer at Ensho Health Inc.",
+    description: `Ensho's goal is to be the first and most effective digital lab. I am in charge of the user's interfaces with the different part of Ensho's systems. I am working with
+        ReactJS, Python (flask), GraphQL and Postgresql.`,
+  },
+  {
+    date: "Mar&nbsp;2018 - Nov&nbsp;2020",
+    title: "Full-Stack Developer at Figure 1 Inc.",
+    description: `Figure 1’s ambition is to help every health care professional around the global achieve clinical mastery. I am part of the developer team, 
+        adding new features and mentoring junior developers. I am working with ReactJs, MongoDB, Go, React Native, and most of the Firebase platform.`,
+  },
+  {
+    date: "Mar&nbsp;2017 - Mar&nbsp;2018",
+    title: "Lead Full-Stack Developer for SIËSDE Technology",
+    description: `I have designed and implemented the first financial disclosure digital solution on the market. It gives lawyers and mediators an easy and 
+        organized way to collect, from their clients, the supporting documents required to endorse a full financial disclosure. 
+        It can be used in cases like couple separations, mortgage applications,...<br/>
+        Technically, it uses NodeJs, Postgresql and Vuejs, the stripe API to collect payments, the IBM Cloud to host our server and AWS object storage 
+        to securely store the documents.<br/>
+        My daily tasks consisted of: managing our team of developers and designers, actively developing new features for our website, recruiting 
+        new employees and freelancers, designing potential solutions based on non-technical specifications, experimenting new technologies...`,
+  },
+  {
+    date: "Mar&nbsp;2017",
+    title: "Winner of HackJustice challenge in Toronto",
+    description: `Conception and implementation of a prototype aiming to inform and guide the user along 
+    a separation process (divorce, etc.)`,
+  },
+  {
+    date: "Aug&nbsp;2016 - Feb&nbsp;2017",
+    title: "CTO at Small Claims Wizard",
+    description:
+      "Involved with technical-related subjects, startup strategy, digital marketing...",
+  },
+  {
+    date: "Feb&nbsp;2016 - Aug&nbsp;2016",
+    title: "Creative Writing (France)",
+    description: "",
+  },
+  {
+    date: "Jul&nbsp;2010 - Feb&nbsp;2016",
+    title: "Business Intelligence consultant, Klee Performance (France)",
+    description: `Use of business intelligence tools (ETL and reporting), technical specifications writing, full BI process conception and
+        implementation (METRO, RATP, Vinci, SFR…), small team management (2 persons)`,
+  },
+];
